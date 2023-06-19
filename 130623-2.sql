c6c04c9b8df221e07fa87ef55d213b65e5afb10c
Natalya <pauls88@yandex.ru>
2023-06-19 23:12:47 +0500

    :AddFile130623-2

---
+++ 130623-2.sql
@@ -0,0 +1,17 @@
+-- create
+CREATE TABLE classmates (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  address TEXT NOT NULL
+);
+
+-- insert
+INSERT INTO classmates VALUES (0001, 'Владислав', 27, 'Москва');
+INSERT INTO classmates VALUES (0002, 'Валентина', 47, 'Москва');
+INSERT INTO classmates VALUES (0003, 'Людмила', 33, 'Санкт-Петербург');
+INSERT INTO classmates VALUES (0004, 'Николай', 42, 'Волгоград');
+INSERT INTO classmates VALUES (0005, 'Виктория', 30, 'Москва');
+INSERT INTO classmates VALUES (0006, 'Елена', 38, 'Роквилл');
+INSERT INTO classmates VALUES (0007, 'Макар', 32, 'Екатеринбург');
+INSERT INTO classmates VALUES (0008, 'Юлия', 47, 'Челябинск');
